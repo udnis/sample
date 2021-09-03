3fbeed5fadf1ffaa5a7608b7c58fabeb49162577
Sinduja K <sinduja.nasa01@gmail.com>
2021-09-03 15:00:49 +0530

    initial

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+	
+	@GetMapping("all")
+	public String welcome() {
+		return "Hello World";
+	}
+
+}
